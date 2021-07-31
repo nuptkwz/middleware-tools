4ee79aff65e1fda83385ade5e1adebda9497b6fe
keweizhou <1514991931@qq.com>
2021-07-31 09:30:09 +0800

    test indexs is exists

--- springboot-es/src/test/java/springboot/es/EsDocumentTests.java
+++ springboot-es/src/test/java/springboot/es/EsDocumentTests.java
@@ -3,16 +3,16 @@ package springboot.es;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -52,11 +52,15 @@ class EsDocumentTests {
         System.out.println(indexResponse.status());
     }
 
-    //测试索引是否存在
+    //测试文档是否存在, get /index/doc/1
     @Test
     void testIndexExists() throws IOException {
-        GetIndexRequest getIndexRequest = new GetIndexRequest("kwz_index");
-        boolean exists = client.indices().exists(getIndexRequest, RequestOptions.DEFAULT);
+        GetRequest getRequest = new GetRequest("kwz_index", "1");
+        //不获取返回的_source上下文了
+        getRequest.fetchSourceContext(new FetchSourceContext(false));
+        getRequest.storedFields("_none_");
+
+        boolean exists = client.exists(getRequest, RequestOptions.DEFAULT);
         System.out.println(exists);
     }
 
